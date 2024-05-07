286a4bdeb90f0855793a19820f6e24d95a0f5b00
SychO <104515373+SychO3@users.noreply.github.com>
2024-05-07 12:54:47 +0800

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Update and install necessary tools
+# Update and install necessary tools and libraries
 apt-get update && apt-get upgrade -y
-apt install -y curl socat vim xz-utils openssl gawk file wget screen build-essential
+apt install -y curl socat vim xz-utils openssl gawk file wget screen build-essential libffi-dev libssl-dev zlib1g-dev
 
 # Start a new screen session
 screen -S os
@@ -24,7 +24,7 @@ echo "正在配置安装 Python 3.11.9..."
 
 # Compile and install (use `-j` to speed up the process based on your CPU core count)
 echo "正在编译安装 Python 3.11.9，这可能需要几分钟的时间..."
-make altinstall -j$(nproc)
+make altinstall -j$(nproc) || echo "编译失败，请检查错误日志和系统配置！"
 
 # Update python and python3 links to the newly installed Python version
 echo "正在更新系统默认 Python 版本..."
